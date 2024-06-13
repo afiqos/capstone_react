d33e652a2d9e038d6a5023fdaf1c812cb36fe11a
afiqos <mdafiqosman@gmail.com>
2024-06-13 20:22:10 +0800

    loading from images stored in backend

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@
 function App() {
   const [imageFile, setImageFile] = useState('');
   const [imagePreview, setImgPreview] = useState('');
+  const [downloadedImage, setDownloadImage] = useState('');
 
   function handleFileChange(e) {
     console.log(e.target.files[0].name);
@@ -35,15 +36,27 @@ function App() {
     }
   }
 
+  async function handleDownload() {
+    try {
+      const response = await fetch('http://localhost:8080/image/253');
+      const data = await response.json();
+      console.log(data);
+      console.log(data['filename']);
+      setDownloadImage(`http://localhost:8080/upload-dir/${data['filename']}`);
+    } catch (error) {
+      console.error('Error: ', error);
+    }
+  }
+
   return (
     <div className="App">
       <img src={"http://localhost:8080/upload-dir/testFile.png"}></img>
       
       <input type="file" name="imageFile" onChange={handleFileChange} />
       <img src={imagePreview}></img>
-      <button onClick={handleClick}>
-        Upload Image
-      </button>
+      <button onClick={handleClick}>Upload Image</button>
+      <button onClick={handleDownload}>Download Latest Image</button>
+      <img src={downloadedImage}></img>
     </div>
   );
 }
