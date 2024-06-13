7f2cd91fc6428b738e52e1b2346780220066d61b
afiqos <mdafiqosman@gmail.com>
2024-06-13 19:03:09 +0800

    Managed to post image onto backend

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ function App() {
   const [imageFile, setImageFile] = useState('');
   const [imagePreview, setImgPreview] = useState('');
 
-  function handleClick() {
-    fetch('http://localhost:8080/')
-  }
-
   function handleFileChange(e) {
     console.log(e.target.files[0].name);
     if (e.target.files && e.target.files[0]) {
@@ -16,6 +12,26 @@ function App() {
       
       setImgPreview(URL.createObjectURL(e.target.files[0]));
       URL.revokeObjectURL(imagePreview);
+    }
+  }
+
+  function handleClick() {
+    // fetch('http://localhost:8080/')
+    sendImage();
+  }
+
+  async function sendImage() {
+    const formData = new FormData();
+    formData.append("file", imageFile);
+
+    try {
+      const response = await fetch("http://localhost:8080/", {
+        method: "POST",
+        body: formData,
+      });
+      console.log(await response.json());
+    } catch (e) {
+      console.error(e);
     }
   }
 
