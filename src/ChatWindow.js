886c716e98f9ddaadb5216be2b2138875f0eef86
afiqos <mdafiqosman@gmail.com>
2024-07-02 08:57:27 +0800

    Added enter key to send message

--- src/ChatWindow.js
+++ src/ChatWindow.js
@@ -51,6 +51,12 @@ function ChatWindow() {
     }
   }
 
+  function handleEnterKeyDown(event) {
+    if (event.key === 'Enter') {
+      handleSendMessage(event);
+    }
+  }
+
   return (
       <div className="ChatWindow flex flex-col bottom-10 h-screen">
         <div className="flex-1 space-y-4 px-4 h-full overflow-y-auto">
@@ -58,7 +64,7 @@ function ChatWindow() {
           <div ref={messagesEndRef} />
         </div>
         <div className="h-10 mb-5 ml-5 mr-5 flex items-center pl-2 border-2 border-gray-100 rounded-xl">
-          <input type="text" placeholder="Type your message..." className="flex-1" value={newMessage} onChange={(e) => { setNewMessage(e.target.value) }}/>
+          <input type="text" placeholder="Type your message..." className="flex-1" value={newMessage} onChange={(e) => { setNewMessage(e.target.value) }} onKeyDown={handleEnterKeyDown}/>
           <button type="submit" className="ml-2 py-2 px-4 bg-blue-500 text-white rounded-xl hover:bg-blue-600 focus:outline-none" onClick={handleSendMessage}>Send</button>
         </div>
 
