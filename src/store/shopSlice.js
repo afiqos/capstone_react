0580d72aeeff6d2a7d53694dc40b6e53019187cd
afiqos <mdafiqosman@gmail.com>
2024-08-02 14:46:41 +0800

    Added slice and store files

---
+++ src/store/shopSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const shopSlice = createSlice({
+    name: 'shop',
+    initialState: {
+        shops: []
+    },
+    reducers: {
+        setShops: (state, action) => {
+            state.shops = action.payload
+        }
+    }
+});
+
+export const { setShops } = shopSlice.actions
+
+export default shopSlice.reducer
+export const selectShops = (state) => state.shop.shops
